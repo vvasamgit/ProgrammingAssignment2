fee5211781533a72dace4be0788112492a2d4cdd
vvasamgit <vvasam@gmail.com>
2017-09-14 22:46:27 -0700

    Initial version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix function  stores matrix object and  inverse of matrix object
+## cacheSolve function will be used to compute inverse of the matrix object stored  
+## in makeCacheMatrix function
+## 
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix allows to store a matrix object and its mean
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverseMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseMatrix <<- NULL
+  }
+  get <- function() x
+  setInverseMatrix<- function(invMatrix) inverseMatrix <<- invMatrix
+  getInverseMatrix<- function() inverseMatrix
+  list(set = set, get = get,
+       setInverseMatrix = setInverseMatrix,
+       getInverseMatrix = getInverseMatrix)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve expects input parameter as a function with matrix object and
+## capability to store matrix inverse. casheSolve funcion assumes that it always gets a 
+## function with matrix object for which inverse matrix can be created.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- x$getInverseMatrix()
+  if(!is.null(inverseMatrix)) {
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  data <- x$get()
+  inverseMatrix <- solve(data, ...)
+  x$setInverseMatrix(inverseMatrix)
+  inverseMatrix
+  
 }
+
+xmat<-matrix( c(1:4),nrow=2,ncol=2)
+
+xmat
+
+xmatc<-makeCacheMatrix(xmat)
+
+xmatc
+
+
+cacheSolve(xmatc)
